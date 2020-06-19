6f56523c60615f9f03e66afa2bdac98f4f12a026
29244 <29244@ma-web.nl>
2020-06-19 17:00:16 +0200

    2.1

---
+++ Assets/Pics/Geupdaten UI!!/tekstbalken/textscript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class textscript : MonoBehaviour
+{
+    public GameObject currenttext;
+    public GameObject nexttext;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Return))
+        {
+            if(currenttext.activeSelf == true)
+            {
+            currenttext.SetActive(false);
+            nexttext.SetActive(true);
+            }
+        }
+    }
+}
