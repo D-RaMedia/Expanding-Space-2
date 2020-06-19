4ceff6e39943f5d8fadc6a61fbe991ba91afc6a8
PAP0 <54799202+PAP0@users.noreply.github.com>
2020-06-19 19:31:45 +0200

    textbalken

--- Assets/Pics/Geupdaten UI!!/tekstbalken/textscript.cs
+++ Assets/Pics/Geupdaten UI!!/tekstbalken/textscript.cs
@@ -7,13 +7,7 @@ public class textscript : MonoBehaviour
 {
     public GameObject currenttext;
     public GameObject nexttext;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
+    
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.Return))
