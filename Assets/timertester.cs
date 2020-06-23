b3e38406bc02c50aae573d1e69a07f0f16d83479
PAP0 <54799202+PAP0@users.noreply.github.com>
2020-06-23 22:24:02 +0200

    star system(kinda)

---
+++ Assets/timertester.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timertester : MonoBehaviour
+{ 
+    public float testtimeleft;
+     
+     void Update()
+     {
+         testtimeleft -= Time.deltaTime;
+         Debug.Log(testtimeleft);
+     }
+}
